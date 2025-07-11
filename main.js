b48f889072aaaf7dfc66dacd09efe01c7bd2402d
exwrds <83130767+exwrds@users.noreply.github.com>
2025-07-11 19:41:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Constant Variables
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -37,32 +38,35 @@ async function processQueue() {
 
     const { req, res } = postQueue.shift();
 
-    try {
-        const { webhookUrl, payload } = req.body;
-        
-        console.log(webhookUrl, payload)
-        
-        if (!webhookUrl || !payload) {
-            res.status(400).json({ error: 'missing webhookUrl or payload' });
-        } else {
-            const response = await axios.post(webhookUrl, payload, {
-                headers: { 'Content-Type': 'application/json' }
-            });
 
-            if (response.status >= 200 && response.status < 300) {
-                res.status(200).json({ status: 'success', code: response.status });
-            } else {
-                console.log(response.data)
-                res.status(response.status).json({ error: 'Webhook returned error', details: response.data });
-            }
-        }
-    } catch (error) {
-        console.error('POST queue err:', error.details);
-        res.status(500).json({ error: error.message });
-    } finally {
-        isProcessing = false;
-        processQueue();
+try {
+    const { webhookUrl, payload } = req.body;
+
+    console.log(webhookUrl, payload);
+
+    if (!webhookUrl || !payload) {
+        return res.status(400).json({ error: 'Missing webhookUrl or payload' });
     }
+
+    const response = await axios.post(webhookUrl, payload, {
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    console.log(response.status, response.data)
+
+    if (response.status >= 200 && response.status < 300) {
+        return res.status(200).json({ status: 'success', code: response.status });
+    } else {
+        console.log(response.data);
+        return res.status(response.status).json({ error: 'Webhook returned error', details: response.data });
+    }
+} catch (error) {
+    console.error('Failed! Queue error:', error).message;
+    return res.status(500).json({ error: error.message || 'Unknown error' });
+} finally {
+    isProcessing = false;
+    processQueue();
+}
 }
 
 app.post('/', (req, res) => {
